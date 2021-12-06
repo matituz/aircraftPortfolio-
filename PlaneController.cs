3bef2a363b4a6a115a4a0adab0dacb746d8df688
matituz <67348488+matituz@users.noreply.github.com>
2021-12-06 17:42:49 +0100

    Add files via upload

---
+++ PlaneController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    [SerializeField] private GameObject aircraftCarrier, landingForward, landingBackward;
+    [SerializeField] private float speed = 100;
+    [SerializeField] private GameObject planeSpawner;
+    private bool caughtAircraftCarrier, inLanding;
+    private GameObject landingTarget;
+    private Vector3 flyTarget;
+    void Update()
+    {
+        if(caughtAircraftCarrier == false)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, flyTarget, step);
+
+        }
+        else
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, landingTarget.transform.position, step);
+            if(landingTarget.transform.position == gameObject.transform.position)
+            {
+                RemovePlane();
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+            if (other.gameObject.tag == "backwardLanding")
+            {
+                if (inLanding == false)
+                {
+                    gameObject.transform.parent = other.transform;
+                    StartLanding(landingForward);
+            }
+            }
+            if (other.gameObject.tag == "forwardLanding")
+            {
+                if (inLanding == false)
+                    {
+                    gameObject.transform.parent = other.transform;
+                    StartLanding(landingBackward);
+                }
+            }
+            if(landingTarget != null)
+            {
+                if (other.gameObject.name == landingTarget.name)
+                {
+                    planeSpawner.GetComponent<Spawner>().ChangeScore();
+                    RemovePlane();
+                }
+            }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "exit")
+        {
+            RemovePlane();
+        }
+    }
+    private void StartLanding(GameObject target)
+    {
+        landingTarget = target;
+        caughtAircraftCarrier = true;
+        gameObject.transform.LookAt(target.transform);
+        gameObject.transform.localPosition = transform.right * 0;
+        inLanding = true;
+    }
+    private void RemovePlane()
+    {
+        gameObject.SetActive(false);
+        planeSpawner.GetComponent<Spawner>().CheckAvaiblePlanes(gameObject);
+    }
+    public void ResetPlaneData()
+    {
+        caughtAircraftCarrier = false;
+        inLanding = false;
+        landingTarget = null;
+        flyTarget = gameObject.transform.position * -10;
+        gameObject.transform.LookAt(aircraftCarrier.transform.position);
+        gameObject.transform.parent = null;
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+using TMPro;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private IntVariable score;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float startTimeToSpawn = 3;
+    private int generateNumberOfPlane;
+    private float timeToSpawn = 3f;
+    [SerializeField] private List<GameObject> avaiblePlanes = new List<GameObject>();
+
+    private void Start ()
+	{
+        Invoke("Finished", timeToSpawn);
+        score.Value = 0;
+	}
+    public void Finished()
+    {
+        SpawnPlane();
+        timeToSpawn = startTimeToSpawn * 0.99f;
+        Invoke("Finished", timeToSpawn);
+    }
+        private void SpawnPlane()
+    {
+        if (avaiblePlanes[0] != null)
+        {
+            generateNumberOfPlane = (int)Random.Range(0, avaiblePlanes.Count);
+            avaiblePlanes[generateNumberOfPlane].SetActive(true);
+            avaiblePlanes[generateNumberOfPlane].transform.position = spawnPoints[(int)Random.Range(0, spawnPoints.Length)].transform.position;
+            avaiblePlanes[generateNumberOfPlane].GetComponent<PlaneController>().ResetPlaneData();
+            avaiblePlanes.RemoveAt(generateNumberOfPlane);
+        }
+        else
+        {
+            timeToSpawn = startTimeToSpawn;
+        }
+    }
+    public void CheckAvaiblePlanes(GameObject addPlane)
+    {
+        avaiblePlanes.Add(addPlane);
+    }
+    public void ChangeScore()
+    {
+        score.Value++;
+        scoreText.text = score.Value.ToString();
+    }
+}
